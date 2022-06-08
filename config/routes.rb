2c753e91726217d1bf9d4474aea511fceb6934af
Marcin Matecki <matecki.m@outlook.com>
2022-06-08 19:18:54 +0200

    New user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
