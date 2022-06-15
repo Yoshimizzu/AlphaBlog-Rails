88d6db2b692f538a915fe82e3fdead99f87cc30d
Marcin Matecki <matecki.m@outlook.com>
2022-06-15 17:59:43 +0200

    Add column admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_161047) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_155323) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_161047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
