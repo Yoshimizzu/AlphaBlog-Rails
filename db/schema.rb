0b62535b96d433121fd4c1aa739471023dd038e3
Marcin Matecki <matecki.m@outlook.com>
2022-06-19 20:26:01 +0200

    Category Model and testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_155323) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_182235) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_155323) do
     t.datetime "updated_at", null: false
     t.text "textarea"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
